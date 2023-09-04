d69a1f215ceeecd1f40e49641f8a59c953591dfb
SalsaGal <fielddashos@gmail.com>
2023-09-04 18:03:06 +1000

    Display window

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 import           Linear
 import qualified SDL
+import Control.Monad
 
 width, height :: Num a => a
 width = 64
@@ -24,9 +25,14 @@ main :: IO ()
 main = withWindow $ \window renderer -> do
   texture <- SDL.createTexture renderer SDL.RGBA8888 SDL.TextureAccessStreaming (V2 width height)
 
-  let loop = do
-      SDL.pollEvents
-      keystate <- SDL.getKeyboardState
-      unless (keyState SDL.ScancodeEscape) loop
+  let loop deltaTime = do
+        SDL.pollEvents
+
+        SDL.present renderer
+
+        keyState <- SDL.getKeyboardState
+        unless (keyState SDL.ScancodeEscape) (loop deltaTime)
+
+  loop 0
 
   SDL.destroyTexture texture
